fa6575f58f02fd59604e190d5fe24dfecfd2e88c
ogunstega <ogunstega@gmail.com>
2020-11-02 00:24:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GT_bank_Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities =
+            {
+                "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"
+            };
+
+            Console.Write("\nLINQ : Display the list according to the length then by name in ascending order : ");
+            Console.Write("\n--------------------------------------------------------------------------------\n");
+            Console.Write("\nThe cities are : 'ROME','LONDON','NAIROBI','CALIFORNIA','ZURICH','NEW DELHI','AMSTERDAM','ABU DHABI','PARIS' \n");
+            Console.Write("\nHere is the arranged list :\n");
+            IEnumerable<string> cityOrder =
+            cities.OrderBy(str => str.Length)
+                            .ThenBy(str => str);
+            foreach (string city in cityOrder)
+                Console.WriteLine(city);
+            Console.ReadLine();
+        }
+    }
+}
